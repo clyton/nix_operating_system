ca2a5cd12fb214c120c8df183d0df7824ab9a79a
clyton <clyton.dantis@gmail.com>
2019-04-26 00:38:29 -0700

    added template main.c file

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[])
+int
+main(int argc, char* argv[])
 {
-    puts("Hello, World!");
-    return 0;
+  exit(0);
 }
